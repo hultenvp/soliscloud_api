c1246a37ba52fa2cd482bd93daf8850bc10068e0
hultenvp <61835400+hultenvp@users.noreply.github.com>
2023-01-20 11:41:36 +0100

    Fix some more pylint errors

--- test/test_exceptions.py
+++ test/test_exceptions.py
@@ -11,34 +11,32 @@ def api_instance():
 
 def test_soliscloud_error(mocker):
     err = api.SoliscloudAPI.SolisCloudError()
-    assert f"{err}"=='SolisCloud API error'
+    assert f"{err}" == 'SolisCloud API error'
     err = api.SoliscloudAPI.SolisCloudError("TEST")
-    assert f"{err}"=='TEST'
+    assert f"{err}" == 'TEST'
 
-    
+
 def test_api_error(mocker):
     err = api.SoliscloudAPI.ApiError()
-    assert f"{err}"=='API returned an error: Undefined API error occurred, error code: Unknown, response: None'
+    assert f"{err}" == 'API returned an error: Undefined API error occurred, error code: Unknown, response: None'
     err = api.SoliscloudAPI.ApiError("TEST")
-    assert f"{err}"=='API returned an error: TEST, error code: Unknown, response: None'
+    assert f"{err}" == 'API returned an error: TEST, error code: Unknown, response: None'
     err = api.SoliscloudAPI.ApiError("TEST", 3, 1)
-    assert f"{err}"=='API returned an error: TEST, error code: 3, response: 1'
+    assert f"{err}" == 'API returned an error: TEST, error code: 3, response: 1'
 
 
 def test_http_error(mocker):
     err = api.SoliscloudAPI.HttpError(408)
     now = datetime.now().strftime("%d-%m-%Y %H:%M GMT")
-    assert f"{err}"==f'Your system time is different from server time, your time is {now}'
+    assert f"{err}" == f'Your system time is different from server time, your time is {now}'
     err = api.SoliscloudAPI.HttpError(502)
-    assert f"{err}"=='Http status code: 502'
+    assert f"{err}" == 'Http status code: 502'
     err = api.SoliscloudAPI.HttpError(502, "TEST")
-    assert f"{err}"=='TEST'
+    assert f"{err}" == 'TEST'
 
-    
+
 def test_timeout_error(mocker):
     err = api.SoliscloudAPI.TimeoutError()
-    assert f"{err}"==f'Timeout error occurred'
+    assert f"{err}" == 'Timeout error occurred
     err = api.SoliscloudAPI.TimeoutError("TEST")
-    assert f"{err}"=='TEST'
-
-
+    assert f"{err}" == 'TEST'
