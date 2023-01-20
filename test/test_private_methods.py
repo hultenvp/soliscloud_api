01c801608ab6dde8b04a9dcd5e84148fcf57c3a7
hultenvp <61835400+hultenvp@users.noreply.github.com>
2023-01-20 11:24:38 +0100

    Fix pylint errors

--- test/test_private_methods.py
+++ test/test_private_methods.py
@@ -25,30 +25,47 @@ def api_instance():
     instance = api.SoliscloudAPI('https://soliscloud_test.com:13333/', 1)
     return instance
 
+
 def test_prepare_header(mocker):
     # Mock datetime to get deterministic result
-    mocker.patch('soliscloud_api.soliscloud_api.SoliscloudAPI._now', return_value=datetime(2023,1,1,tzinfo=timezone.utc))
-    header = api.SoliscloudAPI._prepare_header('1234567891234567890', b'DEADBEEFDEADBEEFDEADBEEFDEADBEEF', {'pageNo': 1, 'pageSize': 100}, 'TEST')
+    mocker.patch('soliscloud_api.soliscloud_api.SoliscloudAPI._now', return_value=datetime(2023, 1, 1, tzinfo=timezone.utc))
+    header = api.SoliscloudAPI._prepare_header(
+        '1234567891234567890',
+        b'DEADBEEFDEADBEEFDEADBEEFDEADBEEF',
+        {'pageNo': 1,'pageSize': 100},
+        'TEST'
+    )
     assert header==VALID_HEADER
 
+    
 @pytest.mark.asyncio
 async def test_post_data_json(api_instance, mocker):
     # Still to do
     pass
 
-@pytest.mark.asyncio
-async def test_get_data(api_instance, mocker):
-    mocker.patch.object(api_instance, '_post_data_json', return_value = VALID_RESPONSE)
-    mocker.patch('soliscloud_api.soliscloud_api.SoliscloudAPI._prepare_header', return_value=VALID_HEADER)
-    result=await api_instance._get_data("/TEST", KEY, SECRET, {'pageNo': 1, 'pageSize': 100})
-    api_instance._post_data_json.assert_called_with('https://soliscloud_test.com:13333/TEST', VALID_HEADER, {'pageNo': 1, 'pageSize': 100})
-    assert result==VALID_RESPONSE
 
 @pytest.mark.asyncio
-async def test_get_records(api_instance, mocker):
-    mocker.patch.object(api_instance, '_post_data_json', return_value = VALID_RESPONSE['data'])
+async def test_get_data(api_instance, mocker):
+    mocker.patch.object(api_instance, '_post_data_json', return_value=VALID_RESPONSE)
     mocker.patch('soliscloud_api.soliscloud_api.SoliscloudAPI._prepare_header', return_value=VALID_HEADER)
-    result=await api_instance._get_records("/TEST", KEY, SECRET, {'pageNo': 1, 'pageSize': 100})
-    api_instance._post_data_json.assert_called_with('https://soliscloud_test.com:13333/TEST', VALID_HEADER, {'pageNo': 1, 'pageSize': 100})
+    result = await api_instance._get_data("/TEST", KEY, SECRET, {'pageNo': 1, 'pageSize': 100})
+    api_instance._post_data_json.assert_called_with(
+        'https://soliscloud_test.com:13333/TEST',
+        VALID_HEADER,
+        {'pageNo': 1, 'pageSize': 100}
+    )
+    assert result==VALID_RESPONSE
+
+    
+@pytest.mark.asyncio
+async def test_get_records(api_instance, mocker):
+    mocker.patch.object(api_instance, '_post_data_json', return_value=VALID_RESPONSE['data'])
+    mocker.patch('soliscloud_api.soliscloud_api.SoliscloudAPI._prepare_header', return_value=VALID_HEADER)
+    result = await api_instance._get_records("/TEST", KEY, SECRET, {'pageNo': 1, 'pageSize': 100})
+    api_instance._post_data_json.assert_called_with(
+        'https://soliscloud_test.com:13333/TEST',
+        VALID_HEADER,
+        {'pageNo': 1, 'pageSize': 100}
+    )
     assert result==VALID_RESPONSE['data']['page']['records']
 
