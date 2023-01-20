51ed952c7a3be25fb719b37747554b50a95130d6
hultenvp <61835400+hultenvp@users.noreply.github.com>
2023-01-20 11:44:36 +0100

    Fix more pylint errors

--- test/test_private_methods.py
+++ test/test_private_methods.py
@@ -1,17 +1,9 @@
 import pytest
 from datetime import datetime
 from datetime import timezone
-from aiohttp import ClientError, ClientSession
-from unittest.mock import patch
 import soliscloud_api.soliscloud_api as api
 
-try:
-    # Mock has been added to stdlib in Python 3.3.
-    from unittest.mock import MagicMock, call
-except ImportError:
-    from mock import MagicMock, call
-
-from .const import KEY, SECRET, NMI, VALID_RESPONSE
+from .const import KEY, SECRET, VALID_RESPONSE
 
 VALID_HEADER = {
     'Content-MD5': 'U0Xj//qmRi3zoyapfAAuXw==',
@@ -19,6 +11,7 @@ VALID_HEADER = {
     'Date': 'Sun, 01 Jan 2023 00:00:00 GMT',
     'Authorization': 'API 1234567891234567890:8+oYgqSEFjxPaHIOgUKSpIdYCGU='
 }
+
 
 @pytest.fixture
 def api_instance():
@@ -32,12 +25,12 @@ def test_prepare_header(mocker):
     header = api.SoliscloudAPI._prepare_header(
         '1234567891234567890',
         b'DEADBEEFDEADBEEFDEADBEEFDEADBEEF',
-        {'pageNo': 1,'pageSize': 100},
+        {'pageNo': 1, 'pageSize': 100},
         'TEST'
     )
-    assert header==VALID_HEADER
+    assert header == VALID_HEADER
 
-    
+
 @pytest.mark.asyncio
 async def test_post_data_json(api_instance, mocker):
     # Still to do
@@ -54,9 +47,9 @@ async def test_get_data(api_instance, mocker):
         VALID_HEADER,
         {'pageNo': 1, 'pageSize': 100}
     )
-    assert result==VALID_RESPONSE
+    assert result == VALID_RESPONSE
 
-    
+
 @pytest.mark.asyncio
 async def test_get_records(api_instance, mocker):
     mocker.patch.object(api_instance, '_post_data_json', return_value=VALID_RESPONSE['data'])
@@ -67,5 +60,4 @@ async def test_get_records(api_instance, mocker):
         VALID_HEADER,
         {'pageNo': 1, 'pageSize': 100}
     )
-    assert result==VALID_RESPONSE['data']['page']['records']
-
+    assert result == VALID_RESPONSE['data']['page']['records']
