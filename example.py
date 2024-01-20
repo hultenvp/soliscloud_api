d2028e8182dba41a3ac29b8ba7897e96f37e7153
hultenvp <61835400+hultenvp@users.noreply.github.com>
2024-01-20 14:16:01 +0100

    Fix pylint

--- example.py
+++ example.py
@@ -28,17 +28,17 @@ async def main():
             station_list = await soliscloud.user_station_list(API_KEY, API_SECRET, page_no=1, page_size=100)
             # Australian accounts require NMI, uncomment if required.
             # station_list = await soliscloud.user_station_list(KEY, SECRET, page_no=1, page_size=100, nmi_code=API_NMI)
-            station_list_json = json.dumps(station_list, indent = 2)
+            station_list_json = json.dumps(station_list, indent=2)
             # Use helper class as alternative
             station_ids = await Helpers.get_station_ids(soliscloud, API_KEY, API_SECRET)
-            # Avoid HTTP Error 429 and limit calls/sec (see issue 8) 
+            # Avoid HTTP Error 429 and limit calls/sec (see issue 8)
             time.sleep(2)
             
             # Get inverters for all stations
             inverter_list = await soliscloud.inverter_list(API_KEY, API_SECRET, page_no=1, page_size=100)
             # Australian accounts require NMI, uncomment if required.
-            #inverter_list = await soliscloud.inverter_list(API_KEY, API_SECRET, page_no=1, page_size=100, nmi_code=API_NMI)
-            inverter_list_json = json.dumps(inverter_list, indent = 2)
+            # inverter_list = await soliscloud.inverter_list(API_KEY, API_SECRET, page_no=1, page_size=100, nmi_code=API_NMI)
+            inverter_list_json = json.dumps(inverter_list, indent=2)
             # Use helper class as alternative
             inverter_ids = await Helpers.get_inverter_ids(soliscloud, API_KEY, API_SECRET)
         except (
