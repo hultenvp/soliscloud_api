737041a5a589bf6955a85cb88d45ce808e1557d3
hultenvp <61835400+hultenvp@users.noreply.github.com>
2025-09-13 15:09:11 +0200

    Fix names

--- test/test_public_methods.py
+++ test/test_public_methods.py
@@ -1,7 +1,21 @@
 import pytest
 import soliscloud_api as api
+from soliscloud_api import SoliscloudError
+from soliscloud_api.client import (
+    COLLECTOR_DETAIL,
+    COLLECTOR_LIST,
+    COLLECTOR_DAY,
+    ALARM_LIST,
+    EPM_LIST,
+    EPM_DETAIL,
+    EPM_DAY,
+    EPM_MONTH,
+    EPM_YEAR,
+    EPM_ALL,
+    WEATHER_LIST,
+    WEATHER_DETAIL,
+)
 
-# from soliscloud_api import *
 from .const import (
     KEY,
     SECRET,
@@ -68,7 +82,7 @@ async def test_collector_list_valid(api_instance, patched_api_paged):
     result = await api_instance.collector_list(KEY, SECRET)
     assert result == VALID_RESPONSE_PAGED_RECORDS
     patched_api_paged._get_records.assert_called_with(
-        api.COLLECTOR_LIST, KEY, SECRET, {'pageNo': 1, 'pageSize': 20})
+        COLLECTOR_LIST, KEY, SECRET, {'pageNo': 1, 'pageSize': 20})
 
     # All arguments filled
     result = await api_instance.collector_list(
@@ -77,7 +91,7 @@ async def test_collector_list_valid(api_instance, patched_api_paged):
         page_no=4, page_size=100, station_id=1000, nmi_code=NMI)
     assert result == VALID_RESPONSE_PAGED_RECORDS
     patched_api_paged._get_records.assert_called_with(
-        api.COLLECTOR_LIST,
+        COLLECTOR_LIST,
         KEY, SECRET,
         {
             'pageNo': 4,
@@ -89,7 +103,7 @@ async def test_collector_list_valid(api_instance, patched_api_paged):
 
 @pytest.mark.asyncio
 async def test_collector_list_invalid_page_size(api_instance):
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.collector_list(KEY, SECRET, page_size=101)
 
 
@@ -100,19 +114,19 @@ async def test_collector_detail_valid(api_instance, patched_api):
         KEY, SECRET, collector_sn=1000)
     assert result == VALID_RESPONSE
     patched_api._get_data.assert_called_with(
-        api.COLLECTOR_DETAIL, KEY, SECRET, {'sn': 1000})
+        COLLECTOR_DETAIL, KEY, SECRET, {'sn': 1000})
 
     result = await api_instance.collector_detail(
         KEY, SECRET, collector_id='1000')
     assert result == VALID_RESPONSE
     patched_api._get_data.assert_called_with(
-        api.COLLECTOR_DETAIL, KEY, SECRET, {'id': '1000'})
+        COLLECTOR_DETAIL, KEY, SECRET, {'id': '1000'})
 
 
 @pytest.mark.asyncio
 async def test_collector_detail_invalid_params(api_instance):
     # ID and SN together
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.collector_detail(
             KEY, SECRET,
             collector_sn=1000, collector_id='1000')
@@ -126,29 +140,29 @@ async def test_collector_day_valid(api_instance, patched_api_list):
         collector_sn=1000, time='2023-01-01', time_zone=1)
     assert result == VALID_RESPONSE_LIST
     patched_api_list._get_data.assert_called_with(
-        api.COLLECTOR_DAY,
+        COLLECTOR_DAY,
         KEY, SECRET,
         {'sn': 1000, 'time': '2023-01-01', 'timeZone': 1})
 
 
 @pytest.mark.asyncio
 async def test_collector_day_invalid_params(api_instance):
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.collector_day(
             KEY, SECRET,
             collector_sn=None, time='2023-01-01', time_zone=1)
 
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.collector_day(
             KEY, SECRET,
             collector_sn='1000', time='2023', time_zone=1)
 
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.collector_day(
             KEY, SECRET,
             collector_sn='1000', time='2023+01-01', time_zone=1)
 
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.collector_day(
             KEY, SECRET,
             collector_sn='1000', time='2023-01+01', time_zone=1)
@@ -162,7 +176,7 @@ async def test_alarm_list_valid(api_instance, patched_api_records):
         station_id='1000', begintime='2022-01-01', endtime='2023-01-01')
     assert result == VALID_RESPONSE_RECORDS
     patched_api_records._get_records.assert_called_with(
-        api.ALARM_LIST,
+        ALARM_LIST,
         KEY, SECRET,
         {
             'pageNo': 1,
@@ -176,7 +190,7 @@ async def test_alarm_list_valid(api_instance, patched_api_records):
         device_sn='1000', begintime='2022-01-01', endtime='2023-01-01')
     assert result == VALID_RESPONSE_RECORDS
     patched_api_records._get_records.assert_called_with(
-        api.ALARM_LIST,
+        ALARM_LIST,
         KEY, SECRET,
         {
             'pageNo': 1,
@@ -195,7 +209,7 @@ async def test_alarm_list_valid(api_instance, patched_api_records):
         nmi_code=NMI)
     assert result == VALID_RESPONSE_RECORDS
     patched_api_records._get_records.assert_called_with(
-        api.ALARM_LIST,
+        ALARM_LIST,
         KEY, SECRET,
         {
             'pageNo': 4,
@@ -209,7 +223,7 @@ async def test_alarm_list_valid(api_instance, patched_api_records):
 @pytest.mark.asyncio
 async def test_alarm_list_invalid_params(api_instance):
     # Wrong page size
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.alarm_list(
             KEY, SECRET,
             page_size=1000,
@@ -217,12 +231,12 @@ async def test_alarm_list_invalid_params(api_instance):
             begintime='2022-01-01',
             endtime='2023-01-01')
     # No Id and no Sn
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.alarm_list(
             KEY, SECRET,
             begintime='2022-01-01', endtime='2023-01-01')
     # Both Id and Sn
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.alarm_list(
             KEY, SECRET,
             begintime='2022-01-01',
@@ -230,36 +244,36 @@ async def test_alarm_list_invalid_params(api_instance):
             station_id='1000',
             device_sn='sn')
     # Illegal begin time
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.alarm_list(
             KEY, SECRET,
             begintime='2022',
             endtime='2023-01-01',
             station_id='1000')
 
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.alarm_list(
             KEY, SECRET,
             begintime='2022+01-01', endtime='2023-01-01', station_id='1000')
 
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.alarm_list(
             KEY, SECRET,
             begintime='2022-01+01',
             endtime='2023-01-01',
             station_id='1000')
     # Illegal end time
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.alarm_list(
             KEY, SECRET,
             begintime='2022-01-01', endtime='2023', station_id='1000')
 
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.alarm_list(
             KEY, SECRET,
             begintime='2022-01-01', endtime='2023+01-01', station_id='1000')
 
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.alarm_list(
             KEY, SECRET,
             begintime='2022-01-01', endtime='2023-01+01', station_id='1000')
@@ -271,7 +285,7 @@ async def test_epm_list_valid(api_instance, patched_api_paged):
     result = await api_instance.epm_list(KEY, SECRET)
     assert result == VALID_RESPONSE_PAGED_RECORDS
     patched_api_paged._get_records.assert_called_with(
-        api.EPM_LIST,
+        EPM_LIST,
         KEY, SECRET,
         {'pageNo': 1, 'pageSize': 20})
 
@@ -280,7 +294,7 @@ async def test_epm_list_valid(api_instance, patched_api_paged):
         page_no=4, page_size=30, station_id='1000')
     assert result == VALID_RESPONSE_PAGED_RECORDS
     patched_api_paged._get_records.assert_called_with(
-        api.EPM_LIST,
+        EPM_LIST,
         KEY, SECRET,
         {'pageNo': 4, 'pageSize': 30, 'stationId': '1000'})
 
@@ -288,7 +302,7 @@ async def test_epm_list_valid(api_instance, patched_api_paged):
 @pytest.mark.asyncio
 async def test_epm_list_invalid_params(api_instance):
     # Wrong page_size
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.epm_list(KEY, SECRET, page_size=1000)
 
 
@@ -298,7 +312,7 @@ async def test_epm_detail(api_instance, patched_api):
     result = await api_instance.epm_detail(KEY, SECRET, epm_sn='sn')
     assert result == VALID_RESPONSE
     patched_api._get_data.assert_called_with(
-        api.EPM_DETAIL,
+        EPM_DETAIL,
         KEY, SECRET,
         {'sn': 'sn'})
 
@@ -311,7 +325,7 @@ async def test_epm_day_valid(api_instance, patched_api):
         searchinfo='info', epm_sn='sn', time='2023-01-01', time_zone=1)
     assert result == VALID_RESPONSE
     patched_api._get_data.assert_called_with(
-        api.EPM_DAY,
+        EPM_DAY,
         KEY, SECRET,
         {
             'searchinfo': 'info',
@@ -324,17 +338,17 @@ async def test_epm_day_valid(api_instance, patched_api):
 @pytest.mark.asyncio
 async def test_epm_day_invalid_params(api_instance):
     # Wrong time format
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.epm_day(
             KEY, SECRET,
             searchinfo='info', epm_sn='sn', time='2023', time_zone=1)
 
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.epm_day(
             KEY, SECRET,
             searchinfo='info', epm_sn='sn', time='2023+01-01', time_zone=1)
 
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.epm_day(
             KEY, SECRET,
             searchinfo='info', epm_sn='sn', time='2023-01+01', time_zone=1)
@@ -347,7 +361,7 @@ async def test_epm_month_valid(api_instance, patched_api_list):
         KEY, SECRET, epm_sn='sn', month='2023-01')
     assert result == VALID_RESPONSE_LIST
     patched_api_list._get_data.assert_called_with(
-        api.EPM_MONTH,
+        EPM_MONTH,
         KEY, SECRET,
         {'sn': 'sn', 'month': '2023-01'})
 
@@ -355,12 +369,12 @@ async def test_epm_month_valid(api_instance, patched_api_list):
 @pytest.mark.asyncio
 async def test_epm_month_invalid_params(api_instance):
     # Wrong time format
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.epm_month(
             KEY, SECRET,
             epm_sn='sn', month='2023')
 
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.epm_month(
             KEY, SECRET,
             epm_sn='sn', month='2023+01')
@@ -374,7 +388,7 @@ async def test_epm_year_valid(api_instance, patched_api_list):
         epm_sn='sn', year='2023')
     assert result == VALID_RESPONSE_LIST
     patched_api_list._get_data.assert_called_with(
-        api.EPM_YEAR,
+        EPM_YEAR,
         KEY, SECRET,
         {'sn': 'sn', 'year': '2023'})
 
@@ -382,7 +396,7 @@ async def test_epm_year_valid(api_instance, patched_api_list):
 @pytest.mark.asyncio
 async def test_epm_year_invalid_params(api_instance):
     # Wrong time format
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.epm_year(
             KEY, SECRET,
             epm_sn='sn', year='22023')
@@ -394,7 +408,7 @@ async def test_epm_all_valid(api_instance, patched_api_list):
     result = await api_instance.epm_all(KEY, SECRET, epm_sn='sn')
     assert result == VALID_RESPONSE_LIST
     patched_api_list._get_data.assert_called_with(
-        api.EPM_ALL, KEY, SECRET, {'sn': 'sn'})
+        EPM_ALL, KEY, SECRET, {'sn': 'sn'})
 
 
 @pytest.mark.asyncio
@@ -403,7 +417,7 @@ async def test_weather_list_valid(api_instance, patched_api_paged):
     result = await api_instance.weather_list(KEY, SECRET)
     assert result == VALID_RESPONSE_PAGED_RECORDS
     patched_api_paged._get_records.assert_called_with(
-        api.WEATHER_LIST, KEY, SECRET, {'pageNo': 1, 'pageSize': 20})
+        WEATHER_LIST, KEY, SECRET, {'pageNo': 1, 'pageSize': 20})
 
     # All arguments filled
     result = await api_instance.weather_list(
@@ -411,7 +425,7 @@ async def test_weather_list_valid(api_instance, patched_api_paged):
         page_no=4, page_size=100, station_id=1000, nmi_code=NMI)
     assert result == VALID_RESPONSE_PAGED_RECORDS
     patched_api_paged._get_records.assert_called_with(
-        api.WEATHER_LIST,
+        WEATHER_LIST,
         KEY, SECRET,
         {
             'pageNo': 4,
@@ -422,7 +436,7 @@ async def test_weather_list_valid(api_instance, patched_api_paged):
 
 @pytest.mark.asyncio
 async def test_weather_list_invalid_page_size(api_instance):
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.weather_list(KEY, SECRET, page_size=101)
 
 
@@ -432,12 +446,12 @@ async def test_weather_detail_valid(api_instance, patched_api):
     result = await api_instance.weather_detail(KEY, SECRET, instrument_sn='sn')
     assert result == VALID_RESPONSE
     patched_api._get_data.assert_called_with(
-        api.WEATHER_DETAIL, KEY, SECRET, {'sn': 'sn'})
+        WEATHER_DETAIL, KEY, SECRET, {'sn': 'sn'})
 
 
 @pytest.mark.asyncio
 async def test_weather_detail_invalid_params(api_instance):
-    with pytest.raises(api.SoliscloudAPI.SolisCloudError):
+    with pytest.raises(SoliscloudError):
         await api_instance.weather_detail(
             KEY, SECRET,
             instrument_sn=None)
