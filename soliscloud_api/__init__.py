dbdd2eb96b52574910e46ca7587576166d990f14
hultenvp <61835400+hultenvp@users.noreply.github.com>
2025-12-04 16:27:33 +0100

    Update version and add type and helper imports

--- soliscloud_api/__init__.py
+++ soliscloud_api/__init__.py
@@ -7,7 +7,16 @@ from .client import SoliscloudAPI  # noqa: F401
 from .client import SoliscloudError, SoliscloudHttpError  # noqa: F401
 from .client import SoliscloudTimeoutError, SoliscloudApiError  # noqa: F401
 from .entities import Plant, Inverter, Collector  # noqa: F401
+from .types import (  # noqa: F401
+    EntityType,
+    State,
+    InverterOfflineState,
+    InverterType,
+    PlantType,
+    CollectorState
+)
+from .helpers import Helpers  # noqa: F401
 
 # VERSION
-VERSION = '1.3.0'
+VERSION = '1.4.0'
 __version__ = VERSION
